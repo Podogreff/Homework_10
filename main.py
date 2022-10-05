bacfaabdc6772e17fce15e14f400a2ff945fc1fe
Podogreff <vladkot92@gmail.com>
2022-10-05 16:00:19 +0300

    add

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hi")
+
+with open("myfile.txt", "w") as file:
+    file.write("Hello file world!\n")
+    file.write("Let's learn Python\n")
+# creates a new file named "myfile.txt"
+
+print(open("myfile.txt").read())
+# Hello file world!
+# Let's learn Python
